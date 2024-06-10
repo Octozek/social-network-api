0f9d4268ee9ef225d9289b1412d0e7af2c0a871a
Octozek <octozek@gmail.com>
2024-06-10 17:19:53 -0500

    Add full CRUD functionality, finalize routes and controllers, and ensure all endpoints are working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,6 +7,14 @@ module.exports = {
       .then(thoughts => res.json(thoughts))
       .catch(err => res.status(500).json(err));
   },
+  
+  getThoughtById(req, res) {
+    Thought.findOne({ _id: req.params.id })
+      .select('-__v')
+      .then(thought => res.json(thought))
+      .catch(err => res.status(500).json(err));
+  },
+  
   createThought(req, res) {
     Thought.create(req.body)
       .then(({ _id }) => {
@@ -18,6 +26,47 @@ module.exports = {
       })
       .then(user => res.json(user))
       .catch(err => res.status(500).json(err));
+  },
+  
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
+      .then(thought => res.json(thought))
+      .catch(err => res.status(500).json(err));
+  },
+  
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.id })
+      .then(thought => {
+        if (!thought) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        return User.findOneAndUpdate(
+          { _id: thought.username },
+          { $pull: { thoughts: req.params.id } },
+          { new: true }
+        );
+      })
+      .then(() => res.json({ message: 'Thought deleted!' }))
+      .catch(err => res.status(500).json(err));
+  },
+  
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { new: true, runValidators: true }
+    )
+      .then(thought => res.json(thought))
+      .catch(err => res.status(500).json(err));
+  },
+  
+  removeReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { new: true }
+    )
+      .then(thought => res.json(thought))
+      .catch(err => res.status(500).json(err));
   }
-  // Other methods to be implemented
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,28 @@
 const router = require('express').Router();
-const { getAllUsers, createUser } = require('../controllers/userController');
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
+} = require('../controllers/userController');
 
 // /api/users
 router.route('/')
   .get(getAllUsers)
   .post(createUser);
+
+// /api/users/:id
+router.route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
 
 module.exports = router;
